abbe4e8b3c41f9a7ae22b887b8c5dc466ba160aa
groovyarea <095201a@naver.com>
2023-06-15 00:04:37 +0900

    feat : health check api

--- src/main/java/com/attiead/gateway/GatewayApplication.java
+++ src/main/java/com/attiead/gateway/GatewayApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+/*
 @EnableEurekaClient
+*/
 @RestController
 public class GatewayApplication {
 
